b7750ddfa19d7005149e9f17df36000e1a83bc22
Osder89 <104263852+osder89@users.noreply.github.com>
2023-09-11 11:26:27 -0400

    d_id

--- readChatBack/d-id.js
+++ readChatBack/d-id.js
@@ -1,43 +1,44 @@
-const sdk = require('api')('@d-id/v4.2.0#1f3wxrelm7g7po0');
+const request = require('request');
 
-sdk.auth('Basic b3NjYXJkZWxyaW84OUBnbWFpbC5jb20:CF8iCJY4nmt5P1KJwTbgK');
-sdk.createTalk({
-  script: {
-    type: 'text',
-    subtitles: true,
-    provider: {
-      type: 'microsoft',
-      voice_id: 'en-US-JennyNeural',
-      voice_config: {style: 'string', rate: '0.5', pitch: '+2st'}
-    },
-    ssml: true,
-    input: 'This is an example text'
+const options = {
+  method: 'POST',
+  url: 'https://api.d-id.com/talks',
+  headers: {
+    accept: 'application/json',
+    'content-type': 'application/json',
+    authorization: 'Basic YjNOallYSmtaV3h5YVc4NE9VQm5iV0ZwYkM1amIyMDpFUEVDUXpDMDJFUHZqb0pNaTVnTl8='
   },
-  config: {
-    logo: {position: [0, 500], url: 'string'},
-    fluent: true,
-    pad_audio: 0,
-    driver_expressions: {
-      expressions: [{start_frame: 0, expression: 'neutral', intensity: 0}],
-      transition_frames: 0
+  body: {
+    script: {
+      type: 'text',
+      //Colocar la frase
+      input: 'hola que haces'
     },
-    align_driver: true,
-    align_expand_factor: 0,
-    auto_match: true,
-    motion_factor: 0,
-    normalization_factor: 0,
-    sharpen: true,
-    stitch: true,
-    result_format: 'mp4'
+    
+    //url del avatar
+    source_url: 's3://d-id-images-prod/google-oauth2|113133489368781835558/img_NaS7SE4WPAJ85DQaYFvNj/WIN_20220206_13_39_16_Pro.jpg'
   },
-  face: {top_left: [0, 0], size: 512},
-  source_url: 'https://path.to.directory/image.jpg',
-  driver_url: 'bank://lively',
-  user_data: 'string',
-  name: 'string',
-  webhook: 'https://host.domain.tld/to/webhook',
-  result_url: 'https://path.to.directory/',
-  persist: true
-})
-  .then(({ data }) => console.log(data))
-  .catch(err => console.error(err));
+  json: true
+};
+
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+   const id_avatar= body.id
+  console.log(id_avatar);
+});
+
+
+const options1 = {
+  method: 'GET',
+  url: 'https://api.d-id.com/talks/'+id_avatar,
+  headers: {
+    accept: 'application/json',
+    authorization: 'Basic YjNOallYSmtaV3h5YVc4NE9VQm5iV0ZwYkM1amIyMDpFUEVDUXpDMDJFUHZqb0pNaTVnTl8='
+  }
+};
+
+request(options1, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  console.log(body);
+});
